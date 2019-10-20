b978d4bafd7fb346c41d45b275622d24d38d6a38
Stan le Punk <stanislasiii@gmail.com>
2019-10-20 18:22:10 +0200

    hard code in webpack mode :(

--- Template_V2/webpack.config.js
+++ Template_V2/webpack.config.js
@@ -1,25 +1,32 @@
 const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    export: './src/sketch.ts',
-  },
-
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
-  },
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ]
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
-  }
+	entry: {
+		export: './src/sketch.ts',
+	},
+	/*
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Mon nom de projet',
+		}),
+	],
+*/
+	devtool: 'inline-source-map',
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/
+			}
+		]
+	},
+	resolve: {
+		extensions: [ '.tsx', '.ts', '.js' ]
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].js',
+	}
 };
